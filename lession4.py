bc9cfcf2171b4686d276d13319fc82ec750e6d68
Petr <petr.myraviov@gmail.com>
2026-07-10 16:23:34 +0600

    Первые уроки Python

---
+++ lession4.py
@@ -0,0 +1,9 @@
+name = "Пётр"
+age = 39
+height = 1.68
+motor = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(motor))
---
+++ lesson1.py
@@ -0,0 +1,12 @@
+
+print("Привет, Пётр")
+
+name = "Пётр"
+age = 39
+height = 168
+salary = 80000
+
+print("Имя", name)
+print("Возраст", age)
+print("Рост", height)
+print("Зарплата", salary)
