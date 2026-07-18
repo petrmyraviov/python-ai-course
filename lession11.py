ff773bdbfa30f005dc9a70f1101a9fb4c7d2f8f1
Пётр <petr.myraviov@gmail.com>
2026-07-18 16:08:07 +0600

    Уроки лекции 11

--- lession11.py
+++ lession11.py
@@ -1,4 +1,11 @@
+# Лекция 11, практика 
 print("Введите имя:")
 name = input()
+print("Количество символов: ", len(name))
+print("Первый символ: ", name[0])
+print("Последний символ: ", name[-1])
+print("Все символы:")
 for letter in name:
     print(letter)
+
+print(f"Слово наоборот:\n{name[::-1]}")
