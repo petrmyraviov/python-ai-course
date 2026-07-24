d2a4dedf04108403c9f661eaa896b35108010cf4
Пётр <petr.myraviov@gmail.com>
2026-07-24 16:00:29 +0600

    Уроки лекции 19

--- lession19.py
+++ lession19.py
@@ -5,9 +5,18 @@ motor = {
     "frequency": 50
 }
 for motors in motor:
-    print(motors)
+    print(motors, motor[motors])
 
 #  Задания 2, Лекция 19
 motor["voltage"] = 220
 for i, motors in enumerate(motor):
-    print(motor)
+    print(motors, motor[motors])
+
+# задания 3, Лекция 19
+motor["temperature"] = 75
+for i, motors in enumerate(motor):
+    print(motors, motor[motors])
+
+#  Задания 4, Лекция 19
+# Почему для хранения характеристик двигателя словарь удобнее, чем список?
+# Потому что словарь имеет и нразвания и значения Индификационый ключ и значения , список только значения и номер индефикотор 
